a999be0eda248d2dda21ad99ad9acee52905407e
Marcus <maoste@kth.se>
2020-09-24 15:11:29 +0200

    Ändrade det till en metod då vi kanske vill använda funktionen

--- src/Tools/countAccepted.py
+++ src/Tools/countAccepted.py
@@ -1,10 +1,12 @@
-import numpy as np
 import pandas as pd
 import pathlib
 
+def count_papers(path):
+    #Path koden som är nedan fungerar, men kanske är bättre att bara ta emot path
+    #path = pathlib.Path.cwd()
+    #path = path.parent.parent / ('data' + path.suffix) / ('meta.csv' + path.suffix)
+    data = pd.read_csv(path)
+    accepted_papers = data.accepted.value_counts()[1]
+    rejected_papers = data.accepted.value_counts()[0]
 
-path = pathlib.Path.cwd()
-path = path.parent.parent / ('data' + path.suffix) / ('meta.csv' + path.suffix)
-data = pd.read_csv(path)
-accepted_papers = data.accepted.value_counts()
-print(accepted_papers)
+    return accepted_papers, rejected_papers
