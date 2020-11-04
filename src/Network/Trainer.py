c08e7430cfb241bd279cb2d63e7b9ec9d6e6c055
Jonas Valfridsson <jonas@valfridsson.net>
2020-11-04 14:14:30 +0100

    remove redundant paranthesis

--- src/Network/Trainer.py
+++ src/Network/Trainer.py
@@ -338,7 +338,7 @@ class Trainer:
             self.hook = m.register_forward_hook(self.hook_fn)
 
         def hook_fn(self, module, input, output):
-            self.features = ((output.cpu()).data).numpy()
+            self.features = (output.cpu()).data.numpy()
 
         def remove(self):
             self.hook.remove()
